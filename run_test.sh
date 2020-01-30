d1851a5b59d159bdea34eef4b07e91c272941b77
Michal Brzezinski <brzezinski@google.com>
2020-01-30 00:55:30 +0000

    Running tests with spoon - support for multiple devices at the same time

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # run_test.sh NUMBER_OF_TESTS_TO_RUN
+# remember to correctly set you ANDROID_HOME path!
 
 for ((i=1; i<=$1; i++)); do
     echo "Running test number $i at $(date +"%T")"
-    ./gradlew connectedAndroidTest --console plain | grep -e "InstrumentationResultParser: OK" -e "BUILD"
+#    ./gradlew connectedAndroidTest --console plain | grep -e "InstrumentationResultParser: OK" -e "BUILD"
+    java -jar spoon-runner-1.7.1-jar-with-dependencies.jar \
+        --apk app/build/outputs/apk/debug/app-debug.apk \
+        --test-apk app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
 done
