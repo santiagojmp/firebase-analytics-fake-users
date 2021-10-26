91fd252a7a37b4f427247d3821d9c7f55f37f565
Santiago Martínez <santiagojmp@google.com>
2021-10-26 10:49:43 +0000

    Updated dependencies and gradle plugin to 7.0.3

--- constant_restarter.sh
+++ constant_restarter.sh
@@ -1,7 +1,7 @@
 sleep_interval=60*60*24 # 24 hours
 while true; 
 do 
-  ./restart_emulators.sh &
+  source ./restart_emulators.sh &
   echo "Going to sleep...";
   sleep $((sleep_interval)); 
   echo "Waking up!";
